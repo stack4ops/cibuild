bc3495cc423f736416a45b8ecafa199d690ad90b
Stefan Schneider <eqsoft4@gmail.com>
2026-03-18 11:49:34 +0100

    replace ___ with - in cosign annatotation keys

--- bin/lib/cosign_annotations.sh
+++ bin/lib/cosign_annotations.sh
@@ -9,7 +9,8 @@ while IFS='=' read -r _key _value; do
   case "$_key" in
     CIBUILD_RELEASE_COSIGN_ANNOTATION_*)
       _name="${_key#CIBUILD_RELEASE_COSIGN_ANNOTATION_}"
-      annotation_key=$(echo "${_name}" | tr '[:upper:]' '[:lower:]' | sed 's/__/./g')
+      annotation_key1=$(echo "${_name}" | tr '[:upper:]' '[:lower:]' | sed 's/___/-/g')
+      annotation_key=$(echo "${annotation_key1}" | tr '[:upper:]' '[:lower:]' | sed 's/__/./g')
       set -- "$@" "-a" "${annotation_key}=${_value}"
       ;;
   esac
