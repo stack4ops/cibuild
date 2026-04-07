0e156e6dfb9bf5823b61d495976d7a0a023df039
Stefan Schneider <eqsoft4@gmail.com>
2026-04-07 21:12:34 +0200

    debug summary write

--- bin/lib/release.sh
+++ bin/lib/release.sh
@@ -271,17 +271,22 @@ cibuild__remove_signatures() {
   for platform in $platforms; do
     platform_name=$(echo "$platform" | tr '/' '-')
     image_digest=$(regctl -v error manifest head "${target_image}:${build_tag}-${platform_name} --platform ${platform}" 2>/dev/null) || true
+    cibuild_log_debug "found platform image ${image_digest}"
     if [ -n "${image_digest:-}" ]; then
       fallback_tag=$(echo "${image_digest}" | sed 's/:/-/')
-      regctl -v error tag rm "${target_image}:${fallback_tag}" 2>/dev/null || true
-      regctl -v error tag rm "${target_image}:${fallback_tag}.sig" 2>/dev/null || true
+      regctl -v error tag rm "${target_image}:${fallback_tag}" 
+      #2>/dev/null || true
+      regctl -v error tag rm "${target_image}:${fallback_tag}.sig" 
+      #2>/dev/null || true
     fi
   done
 
   # index sig
   fallback_tag=$(echo "${index_digest}" | sed 's/:/-/')
-  regctl -v error tag rm "${target_image}:${fallback_tag}" 2>/dev/null || true
-  regctl -v error tag rm "${target_image}:${fallback_tag}.sig" 2>/dev/null || true
+  regctl -v error tag rm "${target_image}:${fallback_tag}" 
+  #2>/dev/null || true
+  regctl -v error tag rm "${target_image}:${fallback_tag}.sig"
+  #2>/dev/null || true
   
   cibuild_log_debug "try to remove dsse referrers for ${target_image}@${index_digest}"
 
